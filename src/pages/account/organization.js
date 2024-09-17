226cb8119e6fe130e63bfc356970876466677fdd
Eyewhy <ivanyapweiwang@gmail.com>
2024-09-17 17:42:59 +0800

    mild styling changes

--- src/pages/account/organization.js
+++ src/pages/account/organization.js
@@ -78,23 +78,21 @@ function OrganizationPage ( {setActivePage }) {
             <p class="lead m-2">Manage Organization</p>
             <div class="d-flex">
                 <p class="m-2">
-                    <b>Biodata Scanned</b><br/>
-                    {data['documents_scanned']}
-                </p>
-                <p class="m-2">
-                    <b>Q&A Refreshed</b><br/>
-                    {data['embeddings_generated']}
-                </p>
-            </div>
-            <div class="d-flex">
-                <p class="m-2">
                     <b>Organization Name</b><br/>
                     {data['name']}
                 </p>
                 <p class="m-2">
                     <b>Organization Passphrase</b><br/>
                     {data['passphrase']}
-                </p>    
+                </p>
+                <p class="m-2 ml-4">
+                    <b>Biodatas Scanned</b><br/>
+                    {data['documents_scanned']}
+                </p>
+                <p class="m-2">
+                    <b>Q&A Refreshes</b><br/>
+                    {data['embeddings_generated']}
+                </p>
             </div>
             <span class="m-2">Provide the organization name & passphrase to users to allow them to join your organization.</span>
             
